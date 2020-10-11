38837272e684f2371151069b49798f14ea54c7d3
Diego Córdova <gatesofsilent@hotmail.com>
2020-10-11 15:52:43 -0500

    svg asilenbept square error fixed

--- src/pages/asilenbept.js
+++ src/pages/asilenbept.js
@@ -6,15 +6,15 @@ class Asilenbept extends React.Component {
     return (
       <main id="asilenbept">
         <div className="cube">
-          <svg width="100vw" height="100vh" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100vw 100vh">
+          <svg width="100vw" height="100vh" xmlns="http://www.w3.org/2000/svg">
             <g>
-              <rect id="pas" className="square" x={`calc(50vw - 10rem + 3rem)`}  y={`calc(50vh - 10rem - 2.4rem)`}/>
+              <rect className="square" x={`calc(50vw - 10rem + 3rem)`}  y={`calc(50vh - 10rem - 2.4rem)`}/>
               <rect className="square" x={`calc(50vw - 10rem - 3rem)`}  y={`calc(50vh - 10rem + 2.4rem)`}/>
 
-              <line className="line" x1={`calc(50vw`} y1={`calc(50vh)`} x2={`calc(80vw`} y2={`calc(80vh)`} />
-              <line className="line" x1={`calc(50vw`} y1={`calc(50vh)`} x2={`calc(80vw`} y2={`calc(80vh)`} />
-              <line className="line" x1={`calc(50vw`} y1={`calc(50vh)`} x2={`calc(80vw`} y2={`calc(80vh)`} />
-              <line className="line" x1={`calc(50vw`} y1={`calc(50vh)`} x2={`calc(80vw`} y2={`calc(80vh)`} />
+              <rect className="square-line" x={`calc(50vw - 10rem - 3rem)`}  y={`calc(50vh - 10rem + 2.4rem)`}/>
+              <rect className="square-line" x={`calc(50vw + 10rem - 3rem)`}  y={`calc(50vh - 10rem + 2.4rem)`}/>
+              <rect className="square-line" x={`calc(50vw - 10rem - 3rem)`}  y={`calc(50vh + 10rem + 2.4rem)`}/>
+              <rect className="square-line" x={`calc(50vw + 10rem - 3rem)`}  y={`calc(50vh + 10rem + 2.4rem)`}/>
             </g>
           </svg>
         </div>
